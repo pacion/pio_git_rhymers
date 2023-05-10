fafe5b88d96044377cdcf7d5fd68604eaa88548f
Patryk Gutowski <37260994+pacion@users.noreply.github.com>
2023-05-10 20:41:39 +0200

    14. Remove unused class

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,27 +7,47 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
-        DefaultRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
-                rhymers[j].countIn(i);
-        
+
+        DefaultRhymer[] rhymers = getDefaultRhymers(factory);
+
+        countInRhymers(rhymers);
+
+        countInWithRandomValues(rhymers);
+
+        printCountOut(rhymers);
+
+        printTotalRejectedCounter(rhymers);
+    }
+
+    private static void printTotalRejectedCounter(DefaultRhymer[] rhymers) {
+        System.out.println("total rejected is "
+                + ((HanoiRhymer) rhymers[3]).reportRejected());
+    }
+
+    private static void printCountOut(DefaultRhymer[] rhymers) {
+        for (DefaultRhymer rhymer : rhymers) {
+            while (!rhymer.callCheck())
+                System.out.print(rhymer.countOut() + "  ");
+            System.out.println();
+        }
+    }
+
+    private static void countInWithRandomValues(DefaultRhymer[] rhymers) {
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < 15; i++)
             rhymers[3].countIn(rn.nextInt(20));
-        
-        for (int i = 0; i < rhymers.length; i++) {
-            while (!rhymers[i].callCheck())
-                System.out.print(rhymers[i].countOut() + "  ");
-            System.out.println();
-        }
-        
-        System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
     }
-    
+
+    private static void countInRhymers(DefaultRhymer[] rhymers) {
+        for (int i = 1; i < 15; i++)
+            for (int j = 0; j < 3; j++)
+                rhymers[j].countIn(i);
+    }
+
+    private static DefaultRhymer[] getDefaultRhymers(Rhymersfactory factory) {
+        DefaultRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
+                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        return rhymers;
+    }
+
 }
